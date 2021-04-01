37eca223c9e84c7d75137bdd8baad295703ab52c
Lewis Isom <lewis.w.isom@gmail.com>
2021-04-01 09:53:16 -0600

    Finished up arays/exercises.rb

--- arrays/exercises.rb
+++ arrays/exercises.rb
@@ -10,3 +10,56 @@ end
 
 # 2
 
+# .delete returns what it just deleted
+
+1
+
+[1, 2, 3]
+
+# 3
+
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr.last.first
+
+# 4 
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+# 3
+
+# Error
+
+# 8
+
+# 5
+
+a = "e"
+b = "A"
+c = nil
+
+# 6
+
+# The problem is
+# names[]
+# is expecting an integer in the brackets.
+# There is a string instead.
+# Replace 'margaret' with its index.
+# names[3] = 'jody'
+
+# 7
+
+my_epic_array = [1, 345, 6534, -3.565]
+my_epic_array.each_with_index do |value, index|
+puts "#{index} ~ #{value}"
+end
+
+# 8 
+
+original_array = [1, 3, 5, 7, 8]
+new_array = original_array.map do |value|
+  value + 2
+end
+p original_array
+p new_array
+
+# DONE
