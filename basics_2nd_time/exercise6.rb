4b9eb7cffb18f56969e857600f5e21c17d50e8bd
Lewis Isom <lewis.w.isom@gmail.com>
2021-04-05 17:48:55 -0600

    Finished doing the 'Basics' section for the 2nd time through this book. Save

--- basics_2nd_time/exercise6.rb
+++ basics_2nd_time/exercise6.rb
@@ -1,2 +1,7 @@
 def squared_floats(input)
-  input # WORK IN PROGRESS
+  input.to_f ** 2
+end
+
+puts squared_floats(3.0)
+puts squared_floats(-3.234)
+puts squared_floats(3.5346)
---
+++ basics_2nd_time/exercise7.rb
@@ -0,0 +1,6 @@
+# What does the following error message tell you?
+
+# It tells you there is a syntax error. Specifically, on line 2, you
+# accidently put a bracket } instead of a parenthesis ) when you were
+# trying to close something. It also tells you that there is nothing in
+# the call stack except the base, <main> (its only on line 2 anyways).
